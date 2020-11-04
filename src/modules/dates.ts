10627d075e19d46b297b3968b9862cc961dd9e6f
mishamyrt <misha@myrt.co>
2020-11-04 21:15:13 +0300

    🦙

--- src/modules/dates.ts
+++ src/modules/dates.ts
@@ -1,31 +1,31 @@
-export const toDays = (time: number): number => 
-    Math.floor(time / 1000 / 60 / 60 / 24)
+export const toDays = (time: number): number =>
+  Math.floor(time / 1000 / 60 / 60 / 24)
 
 const months = [
-    'января',
-    'февраля',
-    'марта',
-    'апреля',
-    'мая',
-    'июня',
-    'июля',
-    'августа',
-    'сентября',
-    'октября',
-    'ноября',
-    'декабря'
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря'
 ]
 
-export function getToday (): Date {
-    const today = new Date()
-    today.setHours(0)
-    return today
+export function getToday(): Date {
+  const today = new Date()
+  today.setHours(0)
+  return today
 }
 
-export function formatDate (date: Date) {
-    const today = getToday()
-    if (toDays(date.getTime()) === toDays(today.getTime())) {
-        return 'сегодня'
-    }
-    return `${date.getDate()} ${months[date.getMonth()]}`
+export function formatDate(date: Date) {
+  const today = getToday()
+  if (toDays(date.getTime()) === toDays(today.getTime())) {
+    return 'сегодня'
+  }
+  return `${date.getDate()} ${months[date.getMonth()]}`
 }
