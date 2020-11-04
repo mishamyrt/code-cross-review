9d5239ae751da5b3aff7472d6c37a6afe97819ff
mishamyrt <misha@myrt.co>
2020-11-05 00:31:52 +0300

    Sort employees

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,18 @@ import App from './App.svelte';
 
 const app = new App({
   target: document.body,
-  props: { employees }
+  props: {
+    employees: employees
+      .sort((a, b) => {
+        if (a < b) {
+          return -1;
+        }
+        if (a > b) {
+          return 1;
+        }
+        return 0;
+      })
+  }
 });
 
 export default app;
