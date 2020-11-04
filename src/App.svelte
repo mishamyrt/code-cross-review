af31c5f7485cd3b70c9d95ccfd4c401e513b8bf9
mishamyrt <misha@myrt.co>
2020-11-04 21:14:12 +0300

    Normilize codestyle

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,54 @@
 <script lang="ts">
-    import { buildPairs } from "./modules/pairs";
-    import Pair from './components/Pair.svelte';
-    import Datepicker from './components/Datepicker.svelte';
-    import { getToday } from "./modules/dates";
+  import { buildPairs } from "./modules/pairs";
+  import Pair from "./components/Pair.svelte";
+  import Datepicker from "./components/Datepicker.svelte";
+  import { getToday } from "./modules/dates";
 
-    export let employees: string[];
-    let date = getToday()
+  export let employees: string[];
+  let date = getToday();
 
-    let hoversCount = 0
+  let hoversCount = 0;
 
-    function handleHover (e: CustomEvent) {
-        if (e.detail.state) {
-            hoversCount++
-        } else {
-            hoversCount--
-        }
+  function handleHover(e: CustomEvent) {
+    if (e.detail.state) {
+      hoversCount++;
+    } else {
+      hoversCount--;
     }
+  }
 
-    $: hovered = hoversCount > 0 
+  $: hovered = hoversCount > 0;
 </script>
 
 <style>
-    main {
-        max-width: 870px;
-        margin: 0 auto;
-    }
+  main {
+    max-width: 870px;
+    margin: 0 auto;
+  }
 
-    h1 {
-        font-size: 60px;
-        line-height: 114%;
-        max-width: 680px;
-    }
+  h1 {
+    font-size: 60px;
+    line-height: 114%;
+    max-width: 680px;
+  }
 
-    .pairs {
-        transition: color 0s ease-out;
-    }
+  .pairs {
+    transition: color 0s ease-out;
+  }
 
-    .pairs.__hovered {
-        color: var(--color-text-silenced);
-    }
+  .pairs.__hovered {
+    color: var(--color-text-silenced);
+  }
 </style>
 
 <main>
-    <h1>На кого асайнить мержи <Datepicker bind:date />?</h1>
-    <div class="{`pairs ${hovered ? '__hovered' : ''}`}">
-        {#each buildPairs(employees, date) as pair}
-            <Pair on:hover={handleHover} whom={pair[0]} who={pair[1]} />
-        {/each}
-    </div>
+  <h1>
+    На кого асайнить мержи
+    <Datepicker bind:date />?
+  </h1>
+  <div class={`pairs ${hovered ? '__hovered' : ''}`}>
+    {#each buildPairs(employees, date) as pair}
+      <Pair on:hover={handleHover} whom={pair[0]} who={pair[1]} />
+    {/each}
+  </div>
 </main>
--- src/components/Datepicker.svelte
+++ src/components/Datepicker.svelte
@@ -1,43 +1,38 @@
 <script lang="ts">
-    import { formatDate } from "../modules/dates";
-    import Datepicker from "svelte-calendar";
+  import { formatDate } from "../modules/dates";
+  import Datepicker from "svelte-calendar";
 
-    export let date = new Date();
-    const startDate = new Date()
-    const endDate = new Date(new Date().getTime()+1000*60*60*24*90)
+  export let date = new Date();
+  const startDate = new Date();
+  const endDate = new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 90);
 </script>
 
 <style>
-    button {
-        cursor: pointer;
-        font-size: 60px;
-        font-weight: bold;
-        background: none;
-        border: none;
-        border-bottom: 2px solid var(--color-link-border);
-        padding: 0;
-        transition: color 0.3s ease-out;
-        color: var(--color-text)
-    }
+  button {
+    cursor: pointer;
+    font-size: 60px;
+    font-weight: bold;
+    background: none;
+    border: none;
+    border-bottom: 2px solid var(--color-link-border);
+    padding: 0;
+    transition: color 0.3s ease-out;
+    color: var(--color-text);
+  }
 
-    button:hover {
-        transition: 0s;
-        color: var(--color-link-hover);
-        border-bottom-color: var(--color-link-hover-border);
-    }
+  button:hover {
+    transition: 0s;
+    color: var(--color-link-hover);
+    border-bottom-color: var(--color-link-hover-border);
+  }
 
-    span {
-        font-size: 14px;
-    }
+  span {
+    font-size: 14px;
+  }
 </style>
 
 <span>
-    <Datepicker
-        bind:selected={date}
-        start={startDate}
-        end={endDate}>
-        <button>
-            {formatDate(date)}
-        </button>
-    </Datepicker>
+  <Datepicker bind:selected={date} start={startDate} end={endDate}>
+    <button> {formatDate(date)} </button>
+  </Datepicker>
 </span>
