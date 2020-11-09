37d255a65cb0dd11431ac7c2b0cfdc82963989e1
mishamyrt <misha@myrt.co>
2020-11-09 21:20:39 +0300

    Rename shift function

--- src/modules/pairs.ts
+++ src/modules/pairs.ts
@@ -2,11 +2,11 @@ import { toDays } from "./dates"
 
 const initialDay = toDays(1603712237980)
 
-const shiftFor = (date: Date, limit: number): number =>
+const calculateShift = (date: Date, limit: number): number =>
   ((toDays(date.getTime()) - initialDay) % (limit - 1)) + 1
 
 export function buildPairs(employees: string[], date: Date): [string, string][] {
-  const shift = shiftFor(date, employees.length)
+  const shift = calculateShift(date, employees.length)
   const pairs = []
   for (let i = 0; i < employees.length; i++) {
     let offset = shift + i
