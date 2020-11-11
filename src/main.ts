fb39ca483a40e54583cfc35f033561d54bba9021
mishamyrt <misha@myrt.co>
2020-11-11 22:18:37 +0300

    🦙

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import employees from '../.team-list.json';
 import App from './App.svelte';
 import { alphabetize } from './modules/sort';
 
-const app = new App({
+export default new App({
   target: document.body,
   props: {
     employees: employees.sort(alphabetize)
   }
 });
 
-export default app;
