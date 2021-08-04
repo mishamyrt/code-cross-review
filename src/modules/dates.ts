c4c83684699b936e3dfe0a52811bbbda7d55cb96
mishamyrt <misha@myrt.co>
2021-08-04 19:56:03 +0400

    dates: Add documentaion

--- src/modules/dates.ts
+++ src/modules/dates.ts
@@ -1,7 +1,7 @@
-export const msToDays = (time: number): number =>
-  Math.floor(time / 1000 / 60 / 60 / 24)
-
-const months = [
+/**
+ * Names of the months of the year
+ */
+const MONTHS_NAMES = [
   'января',
   'февраля',
   'марта',
@@ -16,16 +16,30 @@ const months = [
   'декабря'
 ]
 
+/**
+ * Converts JS timestamp to days
+ * @param time - timestamp in milliseconds
+ */
+export const msToDays = (time: number): number =>
+  Math.floor(time / 1000 / 60 / 60 / 24)
+
+/**
+ * Returns current day object
+ */
 export function getToday(): Date {
   const today = new Date()
   today.setHours(0)
   return today
 }
 
+/**
+ * Formats date string
+ * @param date
+ */
 export function formatDate(date: Date): string {
   const today = getToday()
   if (msToDays(date.getTime()) === msToDays(today.getTime())) {
     return 'сегодня'
   }
-  return `${date.getDate()} ${months[date.getMonth()]}`
+  return `${date.getDate()} ${MONTHS_NAMES[date.getMonth()]}`
 }
